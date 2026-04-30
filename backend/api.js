dc5d7058636b9196307b96435d6ad74722764f3f
yuddys85-ship-it <yuddys85@gmail.com>
2026-04-30 17:51:46 +0700

    Update api.js

--- backend/api.js
+++ backend/api.js
@@ -1,9 +1,1 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.status(200).json({ message: "Backend hidup 🚀" });
-  }
 
-  if (req.method === "POST") {
-    return res.status(200).json({ success: true });
-  }
-}
